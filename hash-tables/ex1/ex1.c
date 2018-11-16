ae379ae6192a43b43f67b98c0d17eee1fc2f7d09
Noah Amar <ne.amar@outlook.com>
2018-11-16 12:53:42 -0600

    ex1.c updated

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,12 +6,28 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *ret = malloc(sizeof(Answer));
 
-  // YOUR CODE HERE
-  HashTable *ht = malloc(sizeof(HashTable));
-  ht->capacity = 16;
-  ht->storage = calloc(ht->capacity, sizeof(LinkedPair *));
-  return NULL;
+  if(length < 2) {
+    return NULL;
+  }
+   for(int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
+  }
+   for(int y = 0; y < length; y++) {
+    int matching = hash_table_retrieve(ht, limit - weights[y]);
+    if(matching != -1) {
+      if(y > matching) {
+        ret->index_1 = y;
+        ret->index_2 = matching;
+      } else {
+        ret->index_1 = matching;
+        ret->index_2 = y;
+      }
+      destroy_hash_table(ht);
+      return ret;
+    }
+  }
 }
 
 void print_answer(Answer *answer)
